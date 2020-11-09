5b040dfb13d26e765bff7eb92bdebb3581f1a9b3
meteh <metehan.ertan@hotmail.com>
2020-11-10 00:39:24 +0300

    added mean of values

--- tutanakCalc.py
+++ tutanakCalc.py
@@ -3,6 +3,7 @@ import re
 
 entries = os.listdir("Tutanak/")
 
+txtcount = 0
 linecount = 0
 wordcount = 0
 speechcount = 0
@@ -11,6 +12,7 @@ for entry in entries:
     if entry.endswith(".txt"):
         file = "Tutanak/" + entry
         if os.path.getsize(file) != 0:
+            txtcount += 1
             f = open(file, encoding="utf-8")
 
             #find line and word count
@@ -30,8 +32,9 @@ for entry in entries:
                 res = len(line.split())
                 wordcount += res
 
-print("linecount: ", linecount)
-print("wordcount: ", wordcount)
-print("BAŞKAN speech: ", speechcount)
-print("other speech: ", reCount)
+print("txtcount: ",txtcount)
+print("linecount: ", linecount, ", ", linecount/txtcount)
+print("wordcount: ", wordcount, ", ", wordcount/txtcount)
+print("BAŞKAN speech: ", speechcount, ", ", speechcount/txtcount)
+print("other speech: ", reCount, ", ", reCount/txtcount)
 
